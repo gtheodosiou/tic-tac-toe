1c5079f8b9acaf31b60ca4d5fcf1d469c9d19e5d
George Theodosiou <theodgeorge@gmail.com>
2019-01-04 11:00:40 +0000

    Export game to a different file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Game from './game'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -10,140 +11,6 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-function calculateWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
-    }
-    return null;
-}
-
-function Square(props) {
-    return (
-        <button className="square" onClick={props.onClick}>
-            {props.value}
-        </button>
-    );
-}
-
-class Board extends React.Component {
-    renderSquare(i) {
-        return (
-            // We’re passing down two props from Board to Square: value and onClick.
-            // The onClick prop is a function that Square can call when clicked.
-            <Square
-                value={this.props.squares[i]}
-                onClick={() => this.props.onClick(i)}
-            />
-        );
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
-    }
-}
-
-class Game extends React.Component {
-    constructor(props) {
-        super(props);
-        // To collect data from multiple children, or to have two child components communicate with each other,
-        // you need to declare the shared state in their parent component instead.
-        // The parent component can pass the state back down to the children by using props;
-        // this keeps the child components in sync with each other and with the parent component.
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true,
-        };
-    }
-
-    render() {
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if (winner) {
-            status = 'Winner: ' + winner;
-        } else if (!this.state.squares.includes(null)) {
-            status = 'It\'s a draw!';
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-
-        return (
-            <div className="game">
-                <h2> Tic Tac Toe </h2>
-                <div className="game-content">
-                    <div className="game-board">
-                        <Board
-                            squares={this.state.squares}
-                            onClick={(i) => this.handleClick(i)}
-                        />
-                    </div>
-                    <div className="game-info">
-                        <div className="reset-game">
-                            <button onClick={this.resetGame}>New Game</button>
-                        </div>
-                        <div className="status">{status}</div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-
-    handleClick(i) {
-        // We use slice to maintain immutability.
-        // The main benefit of immutability is that it helps you build pure components in React.
-        // Immutable data can easily determine if changes have been made
-        // which helps to determine when a component requires re-rendering.
-        const squares = this.state.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-        });
-    }
-
-    resetGame = () => {
-        this.setState({
-            squares: Array(9).fill(null),
-            xIsNext: true,
-        });
-    };
-}
-
-// ========================================
 
 ReactDOM.render(
     <Game />,
